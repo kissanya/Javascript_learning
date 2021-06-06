2b24678956cdb525b31b0f56317d756d51c7e517
kissanya <sandor.kiss@klausensoft.hu>
2021-06-06 16:42:29 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('OK');
+    }, 1000);
+  }).then(value => console.log(`success: ${value}`));
+  
+  ///Promise definiálása és OK elkapás await-tel.
+  const myPromiseAwait = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Timeout ended.');
+      resolve('OK');
+    }, 1000);
+  });
+  
+  async function testAwait() {
+    const value = await myPromiseAwait;
+    console.log(`Await: ${value}`);
+  }
+  testAwait();
+  
